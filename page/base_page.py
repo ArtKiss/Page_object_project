a9341597c31caf9f68467f6013eb25302d5ac2cf
Artur <AKFrosty123@mail.ru>
2022-11-05 18:53:45 +0300

    main_page.py methods move in base_page.py

--- page/base_page.py
+++ page/base_page.py
@@ -1,11 +1,19 @@
 import math
-
+from .locators import BasePageLocators
 from selenium.common import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage():
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
     def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
